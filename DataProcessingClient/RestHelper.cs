c8de040e2a4734d3c551286da12a6129493cd153
Brandon <43607839+brann0n@users.noreply.github.com>
2020-04-04 19:42:11 +0200

    Added a new client project

---
+++ DataProcessingClient/RestHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessingClient
+{
+    public static class RestHelper
+    {
+
+        public static IRestResponse Post(string baseUrl, string resourceUrl, string json)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(resourceUrl, Method.POST);
+            request.Timeout = 20 * 1000; //added extra timeout
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+            request.RequestFormat = DataFormat.Json;
+            return client.Execute(request);
+        }       
+
+
+        public static string ConvertObjectToJson(object arg)
+        {
+            return JsonConvert.SerializeObject(arg);
+        }
+
+        public static T ConvertJsonToObject<T>(string json)
+        {
+            if (json[0] == '<')
+            {
+                return default;
+            }
+
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
